dd25114a272227ea571d8bbe6aa7cc376714556c
Nathaniel Richards <46858886+NateTheDev1@users.noreply.github.com>
2019-11-24 14:20:23 -0500

    Init 1.0

---
+++ app.js
@@ -0,0 +1,59 @@
+window.addEventListener('load', () => {
+	// Grab
+	const image = document.getElementById('image');
+	const weatherDetail = document.getElementById('weather');
+	const temperatureEl = document.getElementById('temperature');
+	const messageEl = document.getElementById('message');
+	let lat;
+	let long;
+
+	//API
+	// let lat = 37.8267;
+	// let long = -122.4233;
+
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
+			const proxy = 'https://cors-anywhere.herokuapp.com/';
+			const API = 'https://api.darksky.net/forecast/05a525f67e1b0e21cb64c25f0002e696/';
+			const url = `${proxy}${API}${lat},${long}`;
+			fetch(url)
+				.then(function(res) {
+					return res.json();
+				})
+				.then(function(data) {
+					console.log(data);
+					//Get data from the currently group from the api data
+					const { temperature, summary, icon } = data.currently;
+
+					let newTemp = Math.floor(temperature);
+
+					temperatureEl.innerHTML = newTemp;
+					weatherDetail.innerHTML = summary;
+					message(temperature);
+				});
+		});
+	}
+
+	function message(temperature) {
+		const coldQuotes = [
+			`Holy fuck it's cold, how are you even alive still?`,
+			`Where are we, fucking Alaska?`,
+			`God dammit I hate the cold..`,
+			`Where the hell is the sun, he must of drank too much again last night...`
+		];
+
+		const warmQuotes = [
+			`Finally some fucking warmth`,
+			`Don't tell Winter but I've been cheating on her with the Sun...`,
+			`Jesus I can feel the sweat dripping off of my balls..`
+		];
+
+		if (temperature < 50) {
+			messageEl.textContent = coldQuotes[Math.floor(Math.random() * coldQuotes.length)];
+		} else if (temperature > 50) {
+			messageEl.textContent = warmQuotes[Math.floor(Math.random() * warmQuotes.length)];
+		}
+	}
+});
