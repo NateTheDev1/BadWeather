2debd56deff2f73e2343ae00a204b4f6dcd8d6db
NateTheDev1 <46858886+NateTheDev1@users.noreply.github.com>
2019-11-24 15:15:22 -0500

    Added Today Section 1.1

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 window.addEventListener('load', () => {
 	// Grab
-	const image = document.getElementById('image');
+	// const image = document.getElementById('image');
 	const weatherDetail = document.getElementById('weather');
 	const temperatureEl = document.getElementById('temperature');
 	const messageEl = document.getElementById('message');
+
+	const todaySummary = document.getElementById('today-summary');
+	const max = document.getElementById('max');
+	const min = document.getElementById('min');
+
 	let lat;
 	let long;
 
@@ -26,12 +31,22 @@ window.addEventListener('load', () => {
 					console.log(data);
 					//Get data from the currently group from the api data
 					const { temperature, summary, icon } = data.currently;
+					const today = data.daily.data;
 
+					//Currently
 					let newTemp = Math.floor(temperature);
-
 					temperatureEl.innerHTML = newTemp;
 					weatherDetail.innerHTML = summary;
 					message(temperature);
+
+					//Today
+					newMax = Math.floor(today[0].temperatureMax);
+					max.innerHTML = newMax;
+					newMin = Math.floor(today[0].temperatureMin);
+					min.innerHTML = newMin;
+					todaySummary.textContent = today[0].summary;
+
+					console.log(today[0].temperatureMax);
 				});
 		});
 	}
